8e54ff3a2a0b8143e03d377a03a894eceff795db
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-24 14:43:48 -0500

    add heroku and add test env

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,18 @@
 const pg = require("pg");
-
-const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
-});
-
+let dbParams = {};
+if (process.env.DATABASE_URL) {
+  dbParams.connectionString = process.env.DATABASE_URL;
+} else {
+  dbParams = {
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+  };
+}
+const client = new pg.Client({ dbParams });
 client
   .connect()
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
-
+  .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
 module.exports = client;
